f1b23dc9cf45fcb5357e15f50728d22107121ad4
KaterynaPryimakova <134732205+KaterynaPryimakova@users.noreply.github.com>
2023-10-30 19:53:00 +0300

    fetch beers done

---
+++ src/cat-api.js
@@ -0,0 +1,42 @@
+const select = document.querySelector('.breed-select');
+const loader = document.querySelector('.loader');
+const errorText = document.querySelector('.error');
+const BASE_URL = 'https://api.thecatapi.com/v1/';
+const ENDPOINT = 'breeds';
+loader.hidden = false;
+errorText.hidden = true;
+export function fetchBreeds() {
+  const option = {
+    method: 'GET',
+    headers: {
+      'x-api-key':
+        'live_yDc4ASp436VjqLpwC5rFwnSPrZ2043WhB43zAEXjdoh2GdulyIea4exNRs8VBGgD',
+    },
+  };
+
+  return fetch(`${BASE_URL}${ENDPOINT}`, option)
+    .then(resp => {
+      if (!resp.ok) {
+        throw new Error(rest.statusText);
+      }
+
+      return resp.json();
+    })
+    .then(breeds => {
+      select.innerHTML = '';
+
+      breeds.forEach(breed => {
+        const selectOption = document.createElement('option');
+        selectOption.value = breed.id;
+        selectOption.textContent = breed.name;
+        select.appendChild(selectOption);
+      });
+
+      loader.hidden = true;
+    })
+    .catch(error => {
+      loader.hidden = true;
+      errorText.hidden = false;
+      console.log(error);
+    });
+}
--- src/index.js
+++ src/index.js
@@ -1,45 +1,7 @@
 import axios from 'axios';
+import { fetchBreeds } from './cat-api';
 
 axios.defaults.headers.common['x-api-key'] =
   'live_yDc4ASp436VjqLpwC5rFwnSPrZ2043WhB43zAEXjdoh2GdulyIea4exNRs8VBGgD';
 
-const BASE_URL = 'https://api.thecatapi.com/v1/';
-const ENDPOINT = 'breeds';
-
-const select = document.querySelector('.breed-select');
-const loader = document.querySelector('.loader');
-const error = document.querySelector('.error');
-// console.dir(select);
-
-// select.addEventListener('click', onSelect);
-
-function fetchBreeds() {
-  const option = {
-    method: 'GET',
-    headers: {
-      'x-api-key':
-        'live_yDc4ASp436VjqLpwC5rFwnSPrZ2043WhB43zAEXjdoh2GdulyIea4exNRs8VBGgD',
-    },
-  };
-
-  return fetch(`${BASE_URL}${ENDPOINT}`, option)
-    .then(resp => {
-      if (!resp.ok) {
-        throw new Error(rest.statusText);
-      }
-
-      return resp.json();
-    })
-    .then(breeds => {
-      select.innerHTML = '';
-
-      breeds.forEach(breed => {
-        const selectOption = document.createElement('option');
-        selectOption.value = breed.id;
-        selectOption.textContent = breed.name;
-        select.appendChild(selectOption);
-      });
-    })
-    .catch(error => console.error(error));
-}
 fetchBreeds();
